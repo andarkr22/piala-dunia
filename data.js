5a25b15feebaa58c961e8e92a8ea00ad2d37bc41
andarkr22 <andarkr4@gmail.com>
2026-06-23 03:35:13 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,11 @@
+export const liveMatches = [
+  { id: 1, home: "Argentina", away: "France", homeScore: 2, awayScore: 1, minute: "76'", status: "LIVE" },
+  { id: 2, home: "Brazil", away: "England", homeScore: 0, awayScore: 0, minute: "12'", status: "LIVE" }
+];
+
+export const playerStats = [
+  { rank: 1, name: "Kylian Mbappé", team: "France", goals: 5, assists: 2 },
+  { rank: 2, name: "Julian Alvarez", team: "Argentina", goals: 4, assists: 1 },
+  { rank: 3, name: "Jude Bellingham", team: "England", goals: 3, assists: 3 },
+  { rank: 4, name: "Vinícius Júnior", team: "Brazil", goals: 3, assists: 2 },
+];
